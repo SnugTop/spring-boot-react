8016dcb0bd8a48ec085a97de72bfab9fa313d335
Cameron Daly <camjdaly@gmail.com>
2025-02-12 16:48:12 -0500

    set up backend with supabase. Able to connect with credentials hardcoded into the application.properties. Cannot figure out how to connect .env file.

--- src/main/java/com/example/spring_boot_react/SpringBootReactApplication.java
+++ src/main/java/com/example/spring_boot_react/SpringBootReactApplication.java
@@ -1,13 +1,21 @@
 package com.example.spring_boot_react;
 
+
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootReactApplication {
+    static {
+        // Load environment variables from .env
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USER"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReactApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactApplication.class, args);
+    }
 }
